fa5e2fed09f519e72cba493bb5c93a95a9ba80a7
julia.apakhova <julia.apakhova@yalantis.com>
2020-06-06 19:54:57 +0300

    added owl carousel to offer page

--- src/js/ui-kit.js
+++ src/js/ui-kit.js
@@ -1,4 +1,1 @@
-@include('js/ui-kit/popup.js')
-@include('js/ui-kit/slider.js')
-@include('js/ui-kit/carusel.js')
-@include('js/ui-kit/go-to-section.js')
+@include('js/ui-kit/carousel.js')
---
+++ src/js/ui-kit/carousel.js
@@ -0,0 +1,20 @@
+(function ($) {
+  $('.js-carousel').owlCarousel({
+    margin:30,
+    nav:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      576:{
+        items:2
+      },
+      992:{
+        items:3
+      },
+      1200:{
+        items:4
+      }
+    }
+  })
+}($));
