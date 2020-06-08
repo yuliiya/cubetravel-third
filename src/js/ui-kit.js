ac84abd8d21a65313ecea90877d80ed292e5e7ff
julia.apakhova <julia.apakhova@yalantis.com>
2020-06-08 10:48:00 +0300

    added datepicker

--- src/js/ui-kit.js
+++ src/js/ui-kit.js
@@ -1,1 +1,2 @@
 @include('js/ui-kit/carousel.js')
+@include('js/ui-kit/datepicker.js')
---
+++ src/js/ui-kit/datepicker.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('.js-date-input-start').datepicker({
+  });
+
+  $('.js-date-input-end').datepicker({
+  });
+});
