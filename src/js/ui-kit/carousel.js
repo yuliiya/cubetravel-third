544120b6aa71a3b5a61564783000b5c72b4bf524
julia.apakhova <julia.apakhova@yalantis.com>
2020-07-30 01:46:49 +0300

    book today page - wip

--- src/js/ui-kit/carousel.js
+++ src/js/ui-kit/carousel.js
@@ -1,24 +1,6 @@
 (function ($) {
-  $('.js-carousel').owlCarousel({
-    margin:30,
-    nav:true,
-    responsive:{
-      0:{
-        items:1
-      },
-      576:{
-        items:2
-      },
-      992:{
-        items:3
-      },
-      1200:{
-        items:4
-      }
-    }
-  })
 
-  $(".owl-stage-outer").attr({
+  $(".js-carousel").attr({
     'data-toggle': 'modal',
     'data-target': '#lightboxModal',
   });
