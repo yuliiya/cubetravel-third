9479302c9258532e645264000e969c3bb2e7bc63
julia.apakhova <julia.apakhova@yalantis.com>
2020-07-31 00:10:26 +0300

    book now section

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,51 @@
-$(function() {
+jQuery( document ).ready( function($) {
 
-  var controller = new ScrollMagic.Controller();
+  var tooSmall = false;
+  var controller = null;
+  var maxWidth = 1100; // or whatever your max width is
 
-  new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 700, offset: 0})
-    .setPin("#pin1")
-    .addTo(controller);
+  if( $(window).width() < maxWidth ) {
+    tooSmall = true;
+  }
 
-  new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 300, })
-    .setPin("#pin2")
-    .addTo(controller);
+  function initScrollMagic() {
+    $('body').scrollTop( 0 );
+    controller = new ScrollMagic.Controller();
+    // add your ScrollMagic scene goodness here
 
-  new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 700, offset: 0})
-    .setPin("#pin3")
-    .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 700, offset: 0})
+      .setPin("#pin1")
+      .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 300, })
+      .setPin("#pin2")
+      .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 700, offset: 0})
+      .setPin("#pin3")
+      .addTo(controller);
+  }
+
+  if( !tooSmall ) {
+    initScrollMagic();
+  }
+
+  // part of the problem is that window resizes can trigger multiple times as the events fire rapidly
+  // this solution prevents the controller from being initialized/destroyed more than once
+  $(window).resize( function() {
+    var wWidth = $(window).width();
+    if( wWidth < maxWidth ) {
+      if( controller !== null && controller !== undefined ) {
+        // completely destroy the controller
+        controller = controller.destroy( true );
+        // if needed, use jQuery to manually remove styles added to DOM elements by GSAP etc. here
+      }
+    } else if( wWidth >= maxWidth ) {
+      if( controller === null || controller === undefined ) {
+        // reinitialize ScrollMagic only if it is not already initialized
+        initScrollMagic();
+      }
+    }
+  });
 
 });
