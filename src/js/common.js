929b9f2e804a0db3eb674bc02662449f7cdbd2c9
julia.apakhova <julia.apakhova@yalantis.com>
2020-07-31 15:15:06 +0300

    changed hero page layout

--- src/js/common.js
+++ src/js/common.js
@@ -21,6 +21,10 @@ jQuery( document ).ready( function($) {
       .setPin("#pin2")
       .addTo(controller);
 
+    new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 300, })
+      .setPin("#pin4")
+      .addTo(controller);
+
     new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 700, offset: 0})
       .setPin("#pin3")
       .addTo(controller);
